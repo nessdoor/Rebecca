101693ce5799093543068a4411b08ca8a32b8fa5
Nessdoor <entropy.overseer@entropic.network>
2023-03-02 03:55:55 +0100

    Implement core conversation completion interface
    Use OpenAI's GPT completion model. Context needs to be prepared manually.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Mozilla Public License Version 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [net.clojars.wkok/openai-clojure "0.3.1"]]
   :repl-options {:init-ns rebecca.core})
--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -1,6 +1,10 @@
-(ns rebecca.core)
+(ns rebecca.core
+  (:require [wkok.openai-clojure.api :as oai]
+            [clojure.string :as cstr]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-reply
+  [context input]
+  (oai/create-completion {:model "text-davinci-003"
+                          :prompt (cstr/join "\n[Other]: " (list context input))
+                          :max-tokens 30
+                          :temperature 0}))
