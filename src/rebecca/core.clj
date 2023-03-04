cc27527b7dd4dfcd23887c566de5c612b0e4f13d
Nessdoor <entropy.overseer@entropic.network>
2023-03-04 04:40:26 +0100

    Factor-out default parameters and parameterize get-reply

--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -1,10 +1,11 @@
 (ns rebecca.core
   (:require [wkok.openai-clojure.api :as oai]))
 
+(def default-parameters {:model "text-davinci-003"
+                         :temperature 0})
+
 (defn get-reply
-  ([context input] (get-reply context "Other" input)) ; Default speaker name is "Other"
-  ([context speaker input]
-  (oai/create-completion {:model "text-davinci-003"
-                          :prompt (format "%s\n[%s]:%s\n[Me]:" context speaker input)
-                          :max-tokens 30
-                          :temperature 0})))
+  [context input & {:keys [speaker] :or {speaker "Other"} :as extra}]  ; Default speaker name is "Other"
+  (oai/create-completion (merge default-parameters
+                                (dissoc extra :speaker)                ; Pass extra parameters to API w/o :speaker
+                                {:prompt (format "%s\n[%s]:%s\n[Me]:" context speaker input)})))
