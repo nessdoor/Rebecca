68bc0e63fb712ed1371f22fbb09e6e7a4fbdeefa
Nessdoor <entropy.overseer@entropic.network>
2023-03-04 02:49:00 +0100

    Use str for concatenation
    Simpler and more direct, without the need to create an ad-hoc collection just
    for concatenation.

--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -1,10 +1,9 @@
 (ns rebecca.core
-  (:require [wkok.openai-clojure.api :as oai]
-            [clojure.string :as cstr]))
+  (:require [wkok.openai-clojure.api :as oai]))
 
 (defn get-reply
   [context input]
   (oai/create-completion {:model "text-davinci-003"
-                          :prompt (cstr/join (list context "\n[Other]:" input "\n[Me]:"))
+                          :prompt (str context "\n[Other]:" input "\n[Me]:")
                           :max-tokens 30
                           :temperature 0}))
