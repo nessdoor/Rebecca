93b3f7c5e0a9d071745a4ff97a5f6cd8cecc6878
Nessdoor <entropy.overseer@entropic.network>
2023-03-06 02:58:16 +0100

    Implement ϵ-extension and use it to implement get-reply
    get-reply is equivalent to the concatenation of a context with an input,
    followed by an ϵ-extension.

--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -4,8 +4,12 @@
 (def default-parameters {:model "text-davinci-003"
                          :temperature 0})
 
+(defn epsilon-extend
+  [history & {:as model-params}]
+  (oai/create-completion (assoc default-parameters :prompt (str history "\n[Me]:"))))
+
 (defn get-reply
-  [context input & {:keys [speaker] :or {speaker "Other"} :as extra}]  ; Default speaker name is "Other"
-  (oai/create-completion (merge default-parameters
-                                (dissoc extra :speaker)                ; Pass extra parameters to API w/o :speaker
-                                {:prompt (format "%s\n[%s]:%s\n[Me]:" context speaker input)})))
+  [context input & {:keys [speaker] :or {speaker "Other"} :as extra}] ; Default speaker name is "Other"
+  (epsilon-extend
+   (format "%s\n[%s]:%s" context speaker input)
+   (dissoc extra :speaker)))         ; Pass extra parameters to API w/o :speaker
