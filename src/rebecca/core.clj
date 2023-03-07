85075b97f43c3b784c7dbe5822f00b6d8587a1d4
Nessdoor <entropy.overseer@entropic.network>
2023-03-07 01:53:29 +0100

    Factor-out default speaker and agent name

--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -4,12 +4,19 @@
 (def default-parameters {:model "text-davinci-003"
                          :temperature 0})
 
+(def default-speaker-name "Other")
+
+(def default-agent-name "Me")
+
 (defn epsilon-extend
   [history & {:as model-params}]
-  (oai/create-completion (assoc default-parameters :prompt (str history "\n[Me]:"))))
+  (oai/create-completion
+   (assoc default-parameters
+          :prompt (str history
+                       (format "\n[%s]:" default-agent-name)))))
 
 (defn get-reply
-  [context input & {:keys [speaker] :or {speaker "Other"} :as extra}] ; Default speaker name is "Other"
+  [context input & {:keys [speaker] :or {speaker default-speaker-name} :as extra}]
   (epsilon-extend
    (format "%s\n[%s]:%s" context speaker input)
    (dissoc extra :speaker)))         ; Pass extra parameters to API w/o :speaker
