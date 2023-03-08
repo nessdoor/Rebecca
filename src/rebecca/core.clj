df23ceb75fd7c0c5f023570cc3cbb1564d48110a
Nessdoor <entropy.overseer@entropic.network>
2023-03-08 02:42:44 +0100

    default-speaker-name -> default-speaker, default-agent-name ->default-agent

--- src/rebecca/core.clj
+++ src/rebecca/core.clj
@@ -1,24 +1,24 @@
 (ns rebecca.core
   (:require [wkok.openai-clojure.api :as oai]))
 
+(def default-speaker "Other")
+
+(def default-agent "Rebecca")
+
 (def default-parameters {:model "text-davinci-003"
                          :temperature 0})
-
-(def default-speaker-name "Other")
-
-(def default-agent-name "Rebecca")
 
 (defn cprime
   [intro participants]
   (str
    intro
-   "\nWhat follows is a conversation between " default-agent-name " and " participants "."))
+   "\nWhat follows is a conversation between " default-agent " and " participants "."))
 
 (defn +facts
-  [history facts] (str default-agent-name " knows that: " facts))
+  [history facts] (str default-agent " knows that: " facts))
 
 (defn +input
-  ([history input] (+input history default-speaker-name input))
+  ([history input] (+input history default-speaker input))
   ([history speaker input] (str history (format "\n[%s]:%s" speaker input))))
 
 (defn epsilon-extend
@@ -27,10 +27,10 @@
    (merge default-parameters
           model-params
           {:prompt (str history
-                        (format "\n[%s]:" default-agent-name))})))
+                        (format "\n[%s]:" default-agent))})))
 
 (defn get-reply
-  [context input & {:keys [speaker] :or {speaker default-speaker-name} :as extra}]
+  [context input & {:keys [speaker] :or {speaker default-speaker} :as extra}]
   (->
    context
    (+input speaker input)
