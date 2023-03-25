2c8d617e8d39d11e3cd65805a7e1b9d27c8d4a76
Nessdoor <entropy.overseer@entropic.network>
2023-03-25 03:01:55 +0100

    Add specs for core data structures

---
+++ src/rebecca/context/spec.clj
@@ -0,0 +1,51 @@
+(ns rebecca.context.spec
+  (:require [clojure.spec.alpha :as s]))
+
+;;; Token count: number of tokens of which a certain text is composed of
+(s/def :rebecca/tokens number?)
+
+;;; Expansion: textual expansion of a segment
+(s/def :rebecca.history/expansion string?)
+
+;;; Component: a textual piece of information said by someone
+(s/def :rebecca.history/speaker string?)
+(s/def :rebecca.history/text string?)
+(s/def :rebecca.history/timestamp inst?)
+(s/def :rebecca.history/component-meta (s/keys :req-un [:rebecca/tokens]
+                                               :opt-un [:rebecca.history/expansion]))
+(s/def :rebecca.history/component (s/keys :req-un [:rebecca.history/text
+                                                   :rebecca.history/timestamp]
+                                          :opt-un [:rebecca.history/speaker]))
+
+;;; Verify whether the given object is a Clojure persistent queue
+(def persistent-queue? #(instance? clojure.lang.PersistentQueue %))
+
+;;; History: a succession of components
+(s/def :rebecca.history/components (s/coll-of
+                                    :rebecca.history/component
+                                    :kind persistent-queue?))
+(s/def :rebecca.history/start-time inst?)
+(s/def :rebecca.history/end-time inst?)
+(s/def :rebecca.history/meta (s/keys :req-un [:rebecca/tokens]))
+(s/def :rebecca/history (s/keys :req-un [:rebecca.history/components
+                                         :rebecca.history/start-time
+                                         :rebecca.history/end-time]))
+
+;;; Context: a (potentially empty) history accompanied by auxiliary information
+(s/def :rebecca.context/agent string?)
+(s/def :rebecca.context/preamble string?)
+(s/def :rebecca.context/tokens-limit number?)
+(s/def :rebecca.context/trim-factor number?)
+(s/def :rebecca.context/tokens-estimator (s/fspec :args (s/cat :t string?)
+                                                  :ret number?))
+(s/def :rebecca.context/pre-toks number?)
+(s/def :rebecca.context/meta (s/keys :req-un [:rebecca/tokens
+                                              :rebecca.context/pre-toks]
+                                     :opt-un [:rebecca.context/tokens-limit
+                                              :rebecca.context/trim-factor
+                                              :rebecca.context/tokens-estimator]))
+(s/def :rebecca/context (s/keys :req-un [:rebecca.context/agent
+                                         :rebecca.context/preamble]
+                                :opt-un [:rebecca.history/components
+                                         :rebecca.history/start-time
+                                         :rebecca.history/end-time]))
