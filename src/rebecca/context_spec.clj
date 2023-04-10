e8d3782b0d87ee270cd2220f65f86cfbbc00a152
Nessdoor <entropy.overseer@entropic.network>
2023-04-10 14:50:16 +0200

    Make contexts a specialization of histories

--- src/rebecca/context_spec.clj
+++ src/rebecca/context_spec.clj
@@ -31,10 +31,9 @@
 (s/def :rebecca.context/agent string?)
 (s/def :rebecca.context/preamble string?)
 (s/def :rebecca.context/pre-toks pos-int?)
-(s/def :rebecca.context/meta (s/keys :req-un [:rebecca/tokens
-                                              :rebecca.context/pre-toks]))
-(s/def :rebecca/context (s/keys :req-un [:rebecca.context/agent
-                                         :rebecca.context/preamble]
-                                :opt-un [:rebecca.history/messages
-                                         :rebecca.history/start-time
-                                         :rebecca.history/end-time]))
+(s/def :rebecca.context/meta (s/merge :rebecca.history/meta
+                                      (s/keys :req-un
+                                              [:rebecca.context/pre-toks])))
+(s/def :rebecca/context (s/merge :rebecca/history
+                                 (s/keys :req-un [:rebecca.context/agent
+                                                  :rebecca.context/preamble])))
