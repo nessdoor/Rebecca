2fe1086305ccd51311fc638755d9d9fccbb8173f
Nessdoor <entropy.overseer@entropic.network>
2023-07-10 02:01:25 +0200

    Update Nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A snowflake for our girl";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default =
